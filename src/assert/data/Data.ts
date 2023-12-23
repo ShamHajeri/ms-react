989785217af926cf83a5dec68d598f1e92280179
Shamsingh Hajeri <105161301+ShamHajeri@users.noreply.github.com>
2023-12-23 13:01:29 +0530

    update

--- src/assert/data/Data.ts
+++ src/assert/data/Data.ts
@@ -11,24 +11,45 @@ const HomeTitle = {
 const EventPage = {
     Event: [{
         "Name": "Space",
-        "url": "www.stenciljs.comLorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur"
+        "url": "www.stenciljs.comLorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur",
+        "description": "dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur",
+        "EIPosition":"10% 45%",
+        "EISize":"10rem"
+        
     },
     {
         "Name": "Planet",
         "url": "www.stenciljs.com",
-        "description": "dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur"
+        "description": "dolor sit, amet consectetur dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur, tempora asperiores consectetur",
+        "EIPosition":"85% 75%",
+        "EISize":"30rem"
+
     },
     {
         "Name": "Planet",
-        "url": "www.stenciljs.com"
+        "url": "www.stenciljs.com",
+        "description": "dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consecteturdolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur",
+        "EIPosition":"70% 45%",
+        "EISize":"40rem"
+
     },
     {
         "Name": "Sreach",
-        "url": "www.google.com"
+        "url": "www.google.com",       
+        "description": "dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consecteturdolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur",
+        "EIPosition":"18% 95%",
+        "EISize":"60rem"
+
+
     },
     {
         "Name": "Contact",
-        "url": "www.google.com"
+        "url": "www.google.com",
+        "description": "dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consecteturdolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consecteturdolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur",
+        "EIPosition":"15% 45%",
+        "EISize":"75rem"
+
+
     },]
 
 }
--- src/componetes/pages/eventPage/eventPage.jsx
+++ src/componetes/pages/eventPage/eventPage.jsx
@@ -2,29 +2,36 @@ import './eventPage.css';
 import NavBar from '../../navBar/navBar';
 import Footer from '../../footer/footer';
 import Data from '../../../assert/data/Data.ts';
+import { useEffect, useState } from 'react';
 
 const EventPage = () => {
-    const data =Data.EventPage
-    console.log(data);
+    const data = Data.EventPage
+    const [eventNumber, setEventNumber] = useState(0);
 
-    const TLHTML=  data.Event.map((item)=><><div class="TLName"><span>{item.Name}</span></div></>);  
+    const TLHTML = data.Event.map((item, index) => <div class="TLName"
+        style={{
+            opacity: `${eventNumber === index ? "100%" : "40%"}`,
+            textAlign: `${eventNumber === index ? "left" : "right"}
+    `}}
+        onClick={() => {
+            setEventNumber(index)
+        }}><span>{item.Name}</span></div>);
+
 
     return (
-    <>
-        <NavBar></NavBar>
-        <div class='event'>
-            <div class="eventDiv">
-                <div class='descriptionBox'>
-                    <span class='description'>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt magni hic ab eaque, tempora asperiores consectetur quam eligendi magnam, optio accusamus qui distinctio sint accusantium blanditiis! Harum eveniet libero sed veniam temporibus ullam delectus nobis doloremque debitis aperiam. Accusamus earum iusto ducimus nemo perspiciatis, distinctio, labore qui amet saepe fugit dolorem ad voluptate blanditiis quam repellendus ex? Ipsam, possimus adipisci assumenda reprehenderit ea aperiam perferendis, corrupti, voluptatum modi molestiae sit quaerat! Earum laboriosam in suscipit totam unde animi nam eius, aliquam laborum minus omnis voluptates nisi sapiente voluptatibus modi? Consequuntur possimus esse voluptates recusandae, tempora vel deserunt ab, magnam facere beatae impedit distinctio, labore culpa. Quas repudiandae assumenda soluta aut ut quibusdam, sit molestias, debitis voluptatibus a explicabo ipsa? Nesciunt quis vitae non! Rerum alias voluptate aperiam perferendis doloremque temporibus repudiandae. Soluta necessitatibus quibusdam provident, eius itaque at odio, ipsa quaerat neque maxime accusantium illum iste ab tempore impedit distinctio corporis non dolorem. Odio, deserunt cum! Animi repellat eligendi error aut, saepe laborum beatae! Pariatur quis vitae commodi, sed dolores quos explicabo, et inventore voluptates ut sequi maxime eos quaerat accusamus blanditiis magnam? Recusandae libero necessitatibus similique corrupti quos obcaecati hic veniam rerum consequuntur repellendus eos molestias labore, id sint!
-                    </span>
+        <>
+            <NavBar></NavBar>
+            <div class='event' style={{ backgroundPosition: `${data.Event[eventNumber].EIPosition}`, backgroundSize:`${data.Event[eventNumber].EISize}` }}>
+                <div class="eventDiv" >
+                    <div class='descriptionBox' >
+                        {data.Event[eventNumber].description}
+                    </div>
+                </div>
+                <div class="timeLine">
+                    {TLHTML}
                 </div>
             </div>
-            <div class="timeLine">
-                {TLHTML}
-            </div>
-        </div>
-        <Footer></Footer>
-    </>)
+            <Footer></Footer>
+        </>)
 }
 export default EventPage;
