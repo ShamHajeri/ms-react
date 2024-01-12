db9ebe1e4d9a3136c381aac230c1ecfde2a6799d
Shamsingh Hajeri <105161301+ShamHajeri@users.noreply.github.com>
2024-01-12 21:31:54 +0530

    12-01

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import {HomePages} from './componetes/pages/homePage/homePage';
 import { Route,Routes } from 'react-router-dom';
 import EventPage from './componetes/pages/eventPage/eventPage';
 import ContactUs from './componetes/pages/contactUs/contactUs';
-import Search from './componetes/search/search';
 import Description from './componetes/pages/description/description';
 import ReviewPage from './componetes/pages/description/reviewPage';
-import Earth from './componetes/solarSysytem/Earth';
 
 
 
@@ -17,7 +15,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<Earth/>}/>
+      <Route path='/' element={<HomePages/>}/>
       <Route path='/home' element={<HomePages/>}/>
       <Route path='/event' element={<EventPage/>}/>
       <Route path='/about' element={<ContactUs/>}/>
