ab5db764c9f4ca697ce6a37e33a265e42bc3f990
Shamsingh Hajeri <105161301+ShamHajeri@users.noreply.github.com>
2024-01-25 15:22:41 +0530

    25-01-2024

--- src/componetes/planet/planet.jsx
+++ src/componetes/planet/planet.jsx
@@ -1,7 +1,6 @@
 import './planet.css'
 const ETB=({etb})=>{
       
-
     return (
         <div class="container-planet">
         <div class="planet-img" style={{backgroundImage:`url("${process.env.PUBLIC_URL}/${etb}.jpg")`}}>
