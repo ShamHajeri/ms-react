6e8dcf81db329841ecacf0bef0c328f6f97d03a1
Shamsingh Hajeri <105161301+ShamHajeri@users.noreply.github.com>
2024-02-08 17:55:54 +0530

    Router Update: redicrecting / to /home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
 import {HomePages} from './componetes/pages/homePage/homePage';
-import { Route,Routes } from 'react-router-dom';
+import { Route,Routes,Navigate } from 'react-router-dom';
 import EventPage from './componetes/pages/eventPage/eventPage';
 import ContactUs from './componetes/pages/contactUs/contactUs';
 import Description from './componetes/pages/description/description';
 import ReviewPage from './componetes/pages/description/reviewPage';
-import ETB from './componetes/planet/planet';
 
 
 function App() {
@@ -15,12 +14,12 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/' element={<ETB etb="earth"/>}/>
       <Route path='/home' element={<HomePages/>}/>
       <Route path='/planet' element={<EventPage/>}/>
       <Route path='/about' element={<ContactUs/>}/>
       <Route path='/dis' element={<Description/>}/>
       <Route path='/Review' element={<ReviewPage/>}/>
+      <Route path="/" element={<Navigate to="/home" />} />
     </Routes>
    
     </>
